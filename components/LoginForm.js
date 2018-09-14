d913cf28c93e3d8441b0b25eb3582ba0645ad6cd
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-09-14 14:42:28 -0300

    Fix directory errors

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { connect } from 'redux';
+import { connect } from 'react-redux';
 import { emailChanged, passwordChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
